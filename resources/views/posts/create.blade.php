bda3771fa17360f096f3bd75c0bed621eeb200c6
Zac <a19860101@gmail.com>
2021-01-04 21:57:39 +0800

    20210104

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,10 @@
 @extends('template.master')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <h2>新增文章</h2>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,10 @@
 @extends('template.master')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <h2>文章列表</h2>
+        </div>
+    </div>
+</div>
+@endsection
