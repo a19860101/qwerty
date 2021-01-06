58c0dee59b81f369597ca855f30c8e6d093ba6cc
Zac <a19860101@gmail.com>
2021-01-06 19:08:06 +0800

    20210106

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,6 +4,19 @@
     <div class="row">
         <div class="col-6 mx-auto">
             <h2>新增文章</h2>
+            <form action="" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="title">文章標題</label>
+                    <input type="text" name="title" id="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="content">內文</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary" value="新增文章">
+                <input type="button" class="btn btn-danger" value="取消" onclick="history.back()">
+            </form>
         </div>
     </div>
 </div>
